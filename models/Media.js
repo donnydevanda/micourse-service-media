ed3d7ea743bcc8713b3c879449b6bb1f9c34967a
Muhammad Donny Devanda <devandadonny@gmail.com>
2021-10-05 21:39:47 +0700

    Create + Read + Delete

--- models/Media.js
+++ models/Media.js
@@ -1,29 +1,32 @@
-const { sequelize } = require(".");
-
-module.exports = (Sequelize, DataTypes) => {
-  const Media = sequelize.define("Media", {
-    id: {
-      type: DataTypes.Integer,
-      primaryKey: true,
-      autoIncrement: true,
-      allowNull: false,
+module.exports = (sequelize, DataTypes) => {
+  const Media = sequelize.define(
+    "Media",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      image: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      createdAt: {
+        field: "created_at",
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      updatedAt: {
+        field: "updated_at",
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
     },
-    image: {
-      type: DataTypes.String,
-      allowNull: false,
-    },
-    createdAt: {
-      field: "created_at",
-      type: DataTypes.DATE,
-      allowNull: false,
-    },
-    updatedAt: {
-      field: "updated_at",
-      type: DataTypes.DATE,
-      allowNull: false,
-    },
-    tableName: "media",
-  });
+    {
+      tableName: "media",
+    }
+  );
 
   return Media;
 };
